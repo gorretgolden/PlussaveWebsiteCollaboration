83a8fd73ce580c2d138e1f0741d6081a18168a21
gorret golden <nabatanzigorret143@gmail.com>
2024-06-05 15:19:50 +0300

    added tailwind css configuration

---
+++ resources/views/components/hero-section.blade.php
@@ -0,0 +1,7 @@
+<!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+<div class="hero" style="background-image: url('{{ $backgroundImage }}');">
+    <div class="hero-text">
+        <h1>{{ $text }}</h1>
+        <a href="#" class="btn">{{ $buttonText }}</a>
+    </div>
+</div>
