2b01a14c4047ed5c85566b5fa8b487488684c2b3
gorret golden <nabatanzigorret143@gmail.com>
2024-07-01 06:14:40 +0300

    added account feature and benefit attributes

--- app/Models/AccountTypeFeature.php
+++ app/Models/AccountTypeFeature.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class AccountTypeFeature extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'account_type_id',
+        'feature',
+    ];
+
+    public function accountType()
+    {
+        return $this->belongsTo(AccountType::class);
+    }
 }
