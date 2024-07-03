0639f9f8bdc8beec20f14d2e717fdef6a11b825d
gorret golden <nabatanzigorret143@gmail.com>
2024-07-03 12:45:45 +0300

    updated account featureas and limits to 4

--- resources/views/livewire/account-types.blade.php
+++ resources/views/livewire/account-types.blade.php
@@ -12,23 +12,31 @@
                     <div>
                         <h3 class="text-xl font-semibold mb-2 text-green-700">Features</h3>
                         <ul class="list-none space-y-3">
-                            @foreach($accountType->features as $feature)
-                            <li class="flex items-center p-2 bg-gray-100 rounded shadow">
-                                <i class="fas fa-check-circle text-green-500 mr-2"></i>
-                                <span class="text-gray-700">{{ $feature->feature }}</span>
-                            </li>
-                            @endforeach
+                            @if ($accountType->features)
+                                @foreach($accountType->features->take(4)  as $feature)
+                                <li class="flex items-center p-2 bg-gray-100 rounded shadow">
+                                    <i class="fas fa-check-circle text-green-500 mr-2"></i>
+                                    <span class="text-gray-700">{{ $feature->feature }}</span>
+                                </li>
+                                @endforeach
+                            @else
+                                <li class="text-gray-700">No features found.</li>
+                            @endif
                         </ul>
                     </div>
                     <div>
                         <h3 class="text-xl font-semibold mb-2 text-green-700">Benefits</h3>
-                        <ul class="list-none space-y-2">
-                            @foreach($accountType->benefits as $benefit)
-                            <li class="flex items-center p-2 bg-gray-100 rounded shadow">
-                                <i class="fas fa-check-circle text-green-500 mr-2"></i>
-                                <span class="text-gray-700">{{ $benefit->benefit }}</span>
-                            </li>
-                            @endforeach
+                        <ul class="list-none space-y-3">
+                            @if ($accountType->benefits)
+                                @foreach($accountType->benefits->take(4)  as $benefit)
+                                <li class="flex items-center p-2 bg-gray-100 rounded shadow">
+                                    <i class="fas fa-check-circle text-green-500 mr-2"></i>
+                                    <span class="text-gray-700">{{ $benefit->benefit }}</span>
+                                </li>
+                                @endforeach
+                            @else
+                                <li class="text-gray-700">No benefits found.</li>
+                            @endif
                         </ul>
                     </div>
                 </div>
