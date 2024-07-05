068c740d881e1f991778d5f80685ce5ea1d67735
gorret golden <nabatanzigorret143@gmail.com>
2024-07-05 06:45:59 +0300

    updated savings plans component

--- app/Http/Livewire/SavingsPlansComponent.php
+++ app/Http/Livewire/SavingsPlansComponent.php
@@ -3,11 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\SavingsPlan;
 
 class SavingsPlansComponent extends Component
 {
+    public $plans;
+
+    public function mount()
+    {
+        $this->plans = SavingsPlan::all();
+    }
+
     public function render()
     {
-        return view('livewire.savings-plans-component');
+        return view('livewire.savings-plans-component', ['plans' => $this->plans]);
     }
 }
--- resources/views/livewire/savings-plans-component.blade.php
+++ resources/views/livewire/savings-plans-component.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<!-- savings-plans-component.blade.php -->
+<div class="p-6">
+    <h2 class="text-2xl font-bold mb-4">Savings Plans</h2>
+    @foreach($plans as $plan)
+        <div class="bg-white shadow-md rounded p-4 mb-4">
+            <h3 class="text-xl font-semibold">{{ $plan->name }}</h3>
+            <p class="text-gray-700">{{ $plan->description }}</p>
+        </div>
+    @endforeach
 </div>
