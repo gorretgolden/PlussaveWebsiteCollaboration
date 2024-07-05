69a716b796df3005a4b076a8fcf971751eef7fc0
gorret golden <nabatanzigorret143@gmail.com>
2024-07-05 06:44:34 +0300

    added financial tips livewire component

--- resources/views/livewire/financial-tips-component.blade.php
+++ resources/views/livewire/financial-tips-component.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<!-- financial-tips-component.blade.php -->
+<div class="p-6">
+    <h2 class="text-2xl font-bold mb-4">Financial Tips and Tricks</h2>
+    @foreach($tips as $tip)
+        <div class="bg-white shadow-md rounded p-4 mb-4">
+            <h3 class="text-xl font-semibold">{{ $tip->title }}</h3>
+            <p class="text-gray-700">{{ $tip->content }}</p>
+        </div>
+    @endforeach
 </div>
