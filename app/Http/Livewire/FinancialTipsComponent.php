9961ae13b294479a354906354a88e786f833bf0b
gorret golden <nabatanzigorret143@gmail.com>
2024-07-05 06:43:40 +0300

    updated financial tips component

--- app/Http/Livewire/FinancialTipsComponent.php
+++ app/Http/Livewire/FinancialTipsComponent.php
@@ -1,13 +1,22 @@
 <?php
 
+
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\FinancialTip;
 
 class FinancialTipsComponent extends Component
 {
+    public $tips;
+
+    public function mount()
+    {
+        $this->tips = FinancialTip::all();
+    }
+
     public function render()
     {
-        return view('livewire.financial-tips-component');
+        return view('livewire.financial-tips-component', ['tips' => $this->tips]);
     }
 }
