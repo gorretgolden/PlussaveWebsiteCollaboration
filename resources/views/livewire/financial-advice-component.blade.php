db2799d157a6ed92100a2c62f2edf87dbbe2d538
gorret golden <nabatanzigorret143@gmail.com>
2024-07-05 07:58:59 +0300

    updated financial literacy sections

--- resources/views/livewire/financial-advice-component.blade.php
+++ resources/views/livewire/financial-advice-component.blade.php
@@ -2,7 +2,7 @@
 
     <!-- financial-advice-component.blade.php -->
 <div class="p-6">
-    <h2 class="text-2xl font-bold mb-4">Financial Advice</h2>
+    <h2 class="text-2xl font-bold mb-4 text-green-700">Financial Advice</h2>
     @foreach($financialAdvice as $advice)
     <div class="mb-4 p-4 border rounded flex items-start">
         <div class="mr-4 text-green-700">
--- resources/views/livewire/financial-tips-component.blade.php
+++ resources/views/livewire/financial-tips-component.blade.php
@@ -1,7 +1,7 @@
 <div >
     <!-- financial-tips-component.blade.php -->
 <div class="p-6">
-    <h2 class="text-2xl font-bold mb-4">Financial Tips and Tricks</h2>
+    <h2 class="text-2xl font-bold mb-4 text-green-700">Financial Tips and Tricks</h2>
     @foreach($financialTips as $tip)
     <div class="mb-4 p-4 border rounded flex items-start">
         <div class="mr-4 text-green-700">
